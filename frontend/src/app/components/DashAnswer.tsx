9c263025c43b9eabbccb0f372d2fce6dd5e44719
Breno Almeida <brenimsilva7@gmail.com>
2023-08-02 06:58:05 -0300

    [Chore]: Colors refactor

--- frontend/src/app/components/DashAnswer.tsx
+++ frontend/src/app/components/DashAnswer.tsx
@@ -6,33 +6,53 @@ type DashAnswerProps = {
   getRightAnswer: () => void;
   answer: IAnswerDTO;
 };
+type Colors = {
+  bgColor: string;
+  shadowColor: string;
+  hoverBgColor: string;
+  textColor: string;
+};
 export default function DashAnswer({
   getAnswerText,
   getRightAnswer,
   answer,
 }: DashAnswerProps) {
-  const colors = [
+  const [colors, setColors] = useState<Colors>({} as Colors);
+  const validColors = [
     "bg-primary",
+    "bg-primary/[0.8]",
+    "bg-primary/[0.5]",
+    "bg-primary/[0.2]",
     "bg-secondary",
     "bg-greenHighlight",
     "text-blueHighLight",
     "text-primary",
     "bg-secondary/[0.4]",
     "shadow-greenHighlight/[0.4]",
+    "shadow-primary/[0.5]",
+    "bg-gray-100",
   ];
+
+  useEffect(() => {
+    answer.RightAnswer === 1
+      ? setColors({
+          bgColor: "primary/[0.5]",
+          hoverBgColor: "primary/[0.2]",
+          shadowColor: "shadow-primary/[0.5]",
+          textColor: "white",
+        })
+      : setColors({
+          bgColor: "white",
+          hoverBgColor: "gray-100",
+          shadowColor: "",
+          textColor: "primary",
+        });
+  }, [answer.RightAnswer]);
   return (
     <div
-      className={`
-      group
-      flex
+      className={`group flex
       justify-center
-      rounded shadow-md ${
-        answer.RightAnswer === 1 ? "shadow-greenHighlight/[0.4]" : ""
-      } p-5 bg-${
-        answer.RightAnswer ? "greenHighlight" : "white"
-      } text-center cursor-pointer hover:bg-${
-        answer.RightAnswer ? "greenHighlight" : "gray-100"
-      } transition`}
+      rounded shadow-md ${colors.shadowColor} border-2 border-secondary p-5 bg-${colors.bgColor} text-center cursor-pointer hover:bg-${colors.hoverBgColor} transition`}
       onClick={() => {
         getRightAnswer();
       }}
@@ -40,13 +60,7 @@ export default function DashAnswer({
       <input
         type="text"
         className={`
-        bg-${
-          answer.RightAnswer ? "greenHighlight" : "white"
-        } focus:outline-none text-${
-          answer.RightAnswer ? "white" : "primary"
-        } text-md rounded-lg  w-80 p-2.5 group-hover:bg-${
-          answer.RightAnswer ? "greenHighlight" : "gray-100"
-        } transition w-fit`}
+        bg-${colors.bgColor} focus:outline-none focu text-${colors.textColor} text-md rounded-lg w-80 p-2.5 group-hover:bg-${colors.hoverBgColor} transition w-fit`}
         placeholder="Insira a resposta..."
         required
         value={answer.AnswerText}
